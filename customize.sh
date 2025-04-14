2b964352219a4adf618615cc746307b3af42e14e
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-14 19:16:58 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
+# Remove default root password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
+# Set default theme to Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# Set default language to auto (remove forced language setting)
+sed -i '/config language/d' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# Set default and permanent TTL to 64
+echo 'net.ipv4.ip_default_ttl=64' >> openwrt/package/base-files/files/etc/sysctl.conf
+
+# Change default SSID to "DCWRT"
+sed -i "s/option ssid.*/option ssid 'DCWRT'/" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Change DISTRIB_DESCRIPTION to DCWRT
+sed -i 's/DISTRIB_DESCRIPTION="LEDE"/DISTRIB_DESCRIPTION="DCWRT"/' openwrt/package/base-files/files/etc/openwrt_release
