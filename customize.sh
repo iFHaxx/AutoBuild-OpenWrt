a6d474aceb945f687a6bac3a35f5e5c018af64b9
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-16 10:02:28 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,14 +6,19 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 # Set default theme to Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-# Set default language to auto (remove forced language setting)
-sed -i '/config language/d' openwrt/package/lean/default-settings/files/zzz-default-settings
+uci -q get system.@imm_init[0] > "/dev/null" || uci -q add system imm_init > "/dev/null"
 
-# Set default and permanent TTL to 64
-echo 'net.ipv4.ip_default_ttl=64' >> openwrt/package/base-files/files/etc/sysctl.conf
+if ! uci -q get system.@imm_init[0].lang > "/dev/null"; then
+	uci -q batch <<-EOF
+		set luci.main.lang="auto"
+		commit luci
 
-# Change default SSID to "DCWRT"
-sed -i "s/option ssid.*/option ssid 'DCWRT'/" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+		set system.@imm_init[0].lang="1"
+		commit system
+	EOF
+fi
 
-# Change DISTRIB_DESCRIPTION to DCWRT
-sed -i 's/DISTRIB_DESCRIPTION="LEDE"/DISTRIB_DESCRIPTION="DCWRT"/' openwrt/package/base-files/files/etc/openwrt_release
+sed -i "/log-facility/d" "/etc/dnsmasq.conf"
+echo "log-facility=/dev/null" >> "/etc/dnsmasq.conf"
+
+ln -sf "/sbin/ip" "/usr/bin/ip"
